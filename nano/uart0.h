7d414a510187686e89f72e1f7698663ac36fbc6e
Aisosa Okunbor <aisosaokunbor@gmail.com>
2026-01-10 09:41:31 -0600

    Update uart0.h

--- nano/uart0.h
+++ nano/uart0.h
@@ -1,5 +1,5 @@
 // UART0 Library
-// Jason Losh
+//Aisosa Okunbor
 
 //-----------------------------------------------------------------------------
 // Hardware Target
@@ -36,3 +36,4 @@ char getcUart0();
 bool kbhitUart0();
 
 #endif
+
